1227dcc4fdcb1296d547d149ad506c7b3d3453c2
김요환 <yh.kim@waveon.kr>
2026-09-17 22:30:07 +0900

    ble 가속도 데이터 추가 ble속도 수정

--- main/ble/ble_task.c
+++ main/ble/ble_task.c
@@ -143,7 +143,8 @@ static void ble_spp_server_advertise(void)
 
     adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
     adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
-
+    adv_params.itvl_max = BLE_GAP_ADV_ITVL_MS(40);
+    adv_params.itvl_min = BLE_GAP_ADV_ITVL_MS(20);
 
     rc = ble_gap_adv_start(
         own_addr_type,
@@ -174,6 +175,14 @@ typedef struct {
 
 client_info_t connected_clients[CONFIG_BT_NIMBLE_MAX_CONNECTIONS];
 
+
+uint16_t GetHandle(uint8_t index)
+{
+    if(index >= CONFIG_BT_NIMBLE_MAX_CONNECTIONS)
+        return 0;
+
+    return connected_clients[index].conn_handle;
+}
 void print_connected_clients(void)
 {
     for (int i = 0; i < CONFIG_BT_NIMBLE_MAX_CONNECTIONS; i++) {
@@ -384,8 +393,8 @@ static int ble_spp_server_gap_event(struct ble_gap_event *event, void *arg)
                 // 2. 클라이언트 목록 배열에 추가!
                 add_client(event->connect.conn_handle, desc.peer_ota_addr.val);
             }
-            ESP_LOGI("BLE_GAP", "현재 연결된 기기 수: %d / %d", 
-                        count_client(), CONFIG_BT_NIMBLE_MAX_CONNECTIONS);
+            ESP_LOGI("BLE_GAP", "현재 연결된 기기 수: %d / %d(%d)", 
+                        count_client(), CONFIG_BT_NIMBLE_MAX_CONNECTIONS,event->connect.conn_handle);
         } 
         delay_adv_timer_cb(NULL);
         #if 0
@@ -886,10 +895,9 @@ void motion_msg_send(uint16_t conn_handle, uint8_t cmd,uint8_t sub_cmd)
             Motion_Packet.ota_req.cmd_type = sub_cmd;
         break;   
         case TIME_RESPONSE:
-            #define KST_OFFSET_SEC  (9 * 3600) // 9시간 (32,400초)
             gettimeofday(&tv, NULL);
             Motion_Packet.event_code = cmd;
-            Motion_Packet.time_res.epoch_sec = tv.tv_sec + KST_OFFSET_SEC;
+            Motion_Packet.time_res.epoch_sec = tv.tv_sec;
         break;   
         default : 
         return;            
--- main/cli/set_cli.c
+++ main/cli/set_cli.c
@@ -11,6 +11,10 @@
 #include "isd2360.h"
 #include "app_feed_motor.h"
 #include "app_button.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
 BaseType_t prvSetInformationCommand( char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString )
 {
     const char *pcParameter;
@@ -140,6 +144,23 @@ BaseType_t prvSetInformationCommand( char *pcWriteBuffer, size_t xWriteBufferLen
 			{
 				Tracker_All_Send(OTA_MODE_REQUEST,1);
 			}
+			else if (!strncmp(ag[1], "bledata", 7))
+			{
+				uint8_t hex_array[20] = {0};
+				uint8_t index = atoi(ag[2]);
+				int len = strlen(ag[3]) / 2;
+				for (size_t i = 0; i < len; i++) {
+					// %2hhx: 2글자씩 읽어서 uint8_t(1바이트) 16진수 값으로 변환
+					sscanf((const char*)&ag[3][i * 2], "%2hhx", &hex_array[i]);
+				}
+				printf("변환 결과: ");
+				for (size_t i = 0; i < len; i++) {
+					printf("0x%02X ", hex_array[i]);
+				}
+				printf("\n");
+				uint16_t handle = GetHandle(index);
+				ble_send_data_to_queue(&handle, (uint8_t*)hex_array,sizeof(hex_array));
+			}	
 			else if (!strncmp(ag[1], "motion", 6))
 			{
 				Tracker_All_Send(MOTION_START_REQUEST,1);
